99701b9c4d069c022ebd4a1804d2ec533722d512
Logan Leyva <logan.a.leyva@gmail.com>
2018-06-09 15:47:18 -0600

    Simple database tests

---
+++ data/src/test/java/test/com/lleyva/chandler/data/config/DataTestConfiguration.java
@@ -0,0 +1,14 @@
+package test.com.lleyva.chandler.data.config;
+
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootConfiguration
+@ComponentScan(basePackages = {"com.lleyva.chandler.data"})
+@PropertySource({"classpath:data.properties", "classpath:postgresql.properties"})
+@EntityScan("com.lleyva.chandler.data")
+@EnableJpaRepositories("com.lleyva.chandler.data.repositories")
+public class DataTestConfiguration { }
---
+++ data/src/test/java/test/com/lleyva/chandler/data/repositories/PostsRepositoryTests.java
@@ -0,0 +1,57 @@
+package test.com.lleyva.chandler.data.repositories;
+
+import com.lleyva.chandler.data.Post;
+import com.lleyva.chandler.data.repositories.PostsRepository;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import test.com.lleyva.chandler.data.config.DataTestConfiguration;
+
+import java.util.Optional;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ContextConfiguration(classes = DataTestConfiguration.class)
+public class PostsRepositoryTests {
+
+	////////////////
+	// Attributes //
+	////////////////
+
+	////////////////
+	// Injections //
+	////////////////
+
+	@Autowired
+	private PostsRepository postsRepository;
+
+	///////////
+	// Tests //
+	///////////
+
+	@Test
+	public void saveAndReadTest() {
+		// Init Test Objects //
+		String testMessage = RandomStringUtils.randomAlphanumeric(100);
+
+		Post post = new Post();
+		post.setMessage(testMessage);
+
+		// Persist and Verify //
+		post = postsRepository.save(post);
+		Assert.assertNotNull(post.getId());
+		Assert.assertEquals(testMessage, post.getMessage());
+
+		Optional<Post> readMessage = postsRepository.findById(post.getId());
+		Assert.assertTrue(readMessage.isPresent());
+		Assert.assertEquals(testMessage, readMessage.get().getMessage());
+	}
+
+}
